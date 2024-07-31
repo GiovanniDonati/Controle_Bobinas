90bc170b54bb68e8b548de41368f11bef77de095
Giovanni_DeskEmp <grdonati99@gmail.com>
2024-07-31 15:30:12 -0300

    Getter e Setter

--- backend/app/models/bobina.py
+++ backend/app/models/bobina.py
@@ -1,18 +1,46 @@
 from datetime import datetime
 
 class Bobina:
-    def __init__(self, endereco, codigo, descricao, lote, metragem, data_entrada):
-        self.endereco = endereco
-        self.codigo = codigo
-        self.descricao = descricao
-        self.lote = lote
-        self.metragem = metragem
-        self.data_entrada = data_entrada
+    def __init__(self, endereco: str, codigo: str, descricao: str, lote: str, metragem: float, data_entrada: datetime):
+        self.__endereco = endereco
+        self.__codigo = codigo
+        self.__descricao = descricao
+        self.__lote = lote
+        self.__metragem = metragem
+        self.__data_entrada = data_entrada
 
-    def alterar_informacoes(self, endereco=None, descricao=None, metragem=None):
-        if endereco:
-            self.endereco = endereco
-        if descricao:
-            self.descricao = descricao
-        if metragem:
-            self.metragem = metragem
+    @property
+    def endereco(self):
+        return self.__endereco
+
+    @endereco.setter
+    def endereco(self, endereco: str):
+        self.__endereco = endereco
+
+    @property
+    def codigo(self):
+        return self.__codigo
+
+    @property
+    def descricao(self):
+        return self.__descricao
+
+    @descricao.setter
+    def descricao(self, descricao: str):
+        self.__descricao = descricao
+
+    @property
+    def lote(self):
+        return self.__lote
+
+    @property
+    def metragem(self):
+        return self.__metragem
+
+    @metragem.setter
+    def metragem(self, metragem: float):
+        self.__metragem = metragem
+
+    @property
+    def data_entrada(self):
+        return self.__data_entrada
--- backend/app/models/estoque.py
+++ backend/app/models/estoque.py
@@ -5,32 +5,37 @@ from datetime import datetime
 
 class Estoque:
     def __init__(self):
-        self.bobinas = []
-        self.historico = Historico()
+        self.__bobinas = []
+        self.__historico = Historico()
 
-    def endereco_ocupado(self, endereco):
-        return any(bobina.endereco == endereco for bobina in self.bobinas)
+    def __endereco_ocupado(self, endereco):
+        return any(bobina.endereco == endereco for bobina in self.__bobinas)
+
+    def __lote_existente(self, lote):
+        return any(bobina.lote == lote for bobina in self.__bobinas)
 
     def adicionar_bobina(self, bobina: Bobina):
-        if self.endereco_ocupado(bobina.endereco):
+        if self.__endereco_ocupado(bobina.endereco):
             raise ValueError(f"Endereço {bobina.endereco} já está ocupado por outra bobina.")
-        self.bobinas.append(bobina)
-        self.historico.adicionar_movimentacao(Movimentacao(bobina, 'entrada', datetime.now(), bobina.endereco))
+        if self.__lote_existente(bobina.lote):
+            raise ValueError(f"Lote {bobina.lote} já existe.")
+        self.__bobinas.append(bobina)
+        self.__historico.adicionar_movimentacao(Movimentacao(bobina, 'entrada', datetime.now(), bobina.endereco))
 
     def remover_bobina(self, codigo):
         bobina = self.buscar_bobina(codigo)
         if bobina:
-            self.bobinas.remove(bobina)
-            self.historico.adicionar_movimentacao(Movimentacao(bobina, 'saída', datetime.now(), 'N/A'))
+            self.__bobinas.remove(bobina)
+            self.__historico.adicionar_movimentacao(Movimentacao(bobina, 'saída', datetime.now(), 'N/A'))
         else:
             raise ValueError(f"Bobina com código {codigo} não encontrada.")
 
     def mover_bobina(self, codigo, novo_endereco):
-        if self.endereco_ocupado(novo_endereco):
+        if self.__endereco_ocupado(novo_endereco):
             raise ValueError(f"Endereço {novo_endereco} já está ocupado por outra bobina.")
         bobina = self.buscar_bobina(codigo)
         if bobina:
-            self.historico.adicionar_movimentacao(Movimentacao(bobina, 'movimentação', datetime.now(), novo_endereco))
+            self.__historico.adicionar_movimentacao(Movimentacao(bobina, 'movimentação', datetime.now(), novo_endereco))
             bobina.endereco = novo_endereco
         else:
             raise ValueError(f"Bobina com código {codigo} não encontrada.")
@@ -38,16 +43,16 @@ class Estoque:
     def mover_para_producao(self, codigo):
         bobina = self.buscar_bobina(codigo)
         if bobina:
-            self.historico.adicionar_movimentacao(Movimentacao(bobina, 'produção', datetime.now(), 'Produção'))
+            self.__historico.adicionar_movimentacao(Movimentacao(bobina, 'produção', datetime.now(), 'Produção'))
             bobina.endereco = 'Produção'
         else:
             raise ValueError(f"Bobina com código {codigo} não encontrada.")
 
     def buscar_bobina(self, codigo):
-        for bobina in self.bobinas:
+        for bobina in self.__bobinas:
             if bobina.codigo == codigo:
                 return bobina
         return None
 
     def obter_historico(self, codigo):
-        return self.historico.obter_historico_por_bobina(codigo)
+        return self.__historico.obter_historico_por_bobina(codigo)
