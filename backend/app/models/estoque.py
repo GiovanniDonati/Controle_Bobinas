a2bf12f6ec3faad26ed809225a77fbac281a54d5
Giovanni_DeskEmp <grdonati99@gmail.com>
2024-07-31 16:30:57 -0300

    inserindo endereços

--- backend/app/models/estoque.py
+++ backend/app/models/estoque.py
@@ -7,6 +7,34 @@ class Estoque:
     def __init__(self):
         self.__bobinas = []
         self.__historico = Historico()
+        
+    def __gerar_enderecos_validos(self):
+        enderecos = []
+        
+        for letra in 'ABCD':
+            for number in range(1,96):
+                enderecos.append(f'{letra}{number}')
+                
+        for number in range(17, 96):
+            enderecos.append(f'E{number}')
+    
+        for number in range(47, 96):
+            enderecos.append(f'F{number}')
+
+        for number in range(82, 96):
+            enderecos.append(f'G{number}')
+
+        for letra in 'ABCDE':
+            for number in range(1, 5):
+                enderecos.append(f'G1-{letra}{number}')
+        
+        for letra in 'ABCD':
+            for number in range(1, 6):
+                enderecos.append(f'G2-{letra}{number}')
+        
+        for letra in 'AB':
+            for number in range(1, 12):
+                enderecos.append(f'G3-{letra}{number}')
 
     def __endereco_ocupado(self, endereco):
         return any(bobina.endereco == endereco for bobina in self.__bobinas)
